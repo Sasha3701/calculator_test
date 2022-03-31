ca59b00cb4bd266d5151a869280916fcf406e18b
Alex Irkhin <irhin_a@garpix.com>
2022-03-31 09:51:22 +0300

    added persist redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import ReactDOM from "react-dom";
 import App from "./App";
-import store from "./store/store";
+import store, { persistor } from "./store/store";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
+import { PersistGate } from "redux-persist/integration/react";
 import "./styles/index.scss";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,54 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistStore,
+  persistReducer,
+} from "redux-persist";
 import calcSlice from "./calcSlice";
 import numSlice from "./numSlice";
 
+const persistConfigNum = {
+  key: "root",
+  storage,
+  blacklist: ['value']
+};
+
+const persistedReducerNum = persistReducer(persistConfigNum, numSlice);
+
+const reducers = combineReducers({
+  calc: calcSlice,
+  num: persistedReducerNum,
+});
+
+const persistConfigRoot = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfigRoot, reducers);
+
 const store = configureStore({
-  reducer: {
-    calc: calcSlice,
-    num: numSlice
-  },
+  reducer: persistedReducer,
+  middleware: [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  ],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
 
 export default store;
